e9607aa2e3ea1463ad5834adeeb962e44e62e3a7
john kenneth dalisay <johnkennethdalisay9@gmail.com>
2024-12-13 03:41:21 +0800

    chnaging texts

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Include the input-output stream library
+
+// Main function: the entry point of the program
+int main() {
+    // Print "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
